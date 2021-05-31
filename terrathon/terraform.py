1631b98d4b679906abc5859007616fc414fe9044
semverbot <semverbot@github.com>
2021-05-31 14:05:41 +0200

    [feature] integrated cliwrap - updated semverbot detection config - updated gitignore

--- terrathon/terraform.py
+++ terrathon/terraform.py
@@ -1,23 +1,9 @@
-import logging
-import subprocess
-
-from terrathon import utils
+from cliwrap import cliwrap, utils
 
 
 def run(subcommand, options, on_success=utils.do_nothing, on_error=utils.do_nothing):
-    try:
-        command = ['terraform', subcommand]
-        command.extend(options)
-
-        logging.info(f'running {command}')
-
-        subprocess.run(command, check=True)
-
-        on_success()
-        return True
-    except subprocess.CalledProcessError as e:
-        on_error()
-        return False
+    command = ['terraform', subcommand]
+    return cliwrap.run(command=command, options=options, on_success=on_success, on_error=on_error)
 
 
 def run_or_exit(subcommand, options, on_success=utils.do_nothing, on_error=utils.do_nothing):
