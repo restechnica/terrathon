cd6e0c3442407c7a07fc57315f02e790b0f475b7
Olivier Van Aken <olivier.va@outlook.com>
2021-06-23 01:14:59 +0200

    fixed lower and upper bounds for cliwrap

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='terrathon',
     description='Lightweight Python wrapper around the Terraform CLI',
     install_requires=[
-        "cliwrap>=0.1.2,<2",
+        "cliwrap>=0.1.1,<1",
     ],
     license="Mozilla Public License Version 2.0",
     long_description=long_description(),
